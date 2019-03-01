67b30c4a13190b191febcf788c3d08198d67ff6a
MAXON-BY <maxon.pro2015@gmail.com>
2019-03-01 18:21:51 +0300

    Create react-kabzda

---
+++ src/Components/Profile.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Profile = () => {
+    return(
+        <div className='content'>
+            <div>
+                <img
+                    src="https://uguide.ru/_nw/1/s58303849.jpg"
+                    alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New Post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
