46c5c24abffd934ab3c78fed3edbb6474865f0f0
maxon-by <maxon.pro2015@gmail.com>
2019-03-16 01:18:59 +0300

    37 video

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,51 +1,58 @@
-let rerendeerEntireTree = () => {
-    console.log('State change')
-}
-
-let state = {
-    profilePage: {
-        postData : [
-            {id:1, message:'First Post', likesCount:15},
-            {id:2, message:'This post is 2, not 1', likesCount:5},
-        ],
-        newPostText: 'Max? Hello!'
+let store = {
+    _state : {
+        profilePage: {
+            postData: [
+                {id: 1, message: 'First Post', likesCount: 15},
+                {id: 2, message: 'This post is 2, not 1', likesCount: 5},
+            ],
+            newPostText: 'Max? Hello!'
+        },
+        dialogsPage: {
+            messages: [
+                {id: 1, message: 'Hello!'},
+                {id: 2, message: 'Yo!'},
+                {id: 3, message: 'Fuck You!'},
+            ],
+            dialogs: [
+                {id: 1, name: 'Igor'},
+                {id: 2, name: 'Max'},
+                {id: 3, name: 'Valera'},
+                {id: 4, name: 'Kate'},
+                {id: 5, name: 'Maxon'},
+                {id: 6, name: 'Jane'},
+                {id: 7, name: 'Kill'},
+                {id: 8, name: 'San'},
+                {id: 9, name: 'Vasia'},
+                {id: 10, name: 'Gopnik'},
+            ]
+        },
+        sidebar: {}
     },
-    dialogsPage: {
-        messages : [
-            {id:1, message:'Hello!'},
-            {id:2, message:'Yo!'},
-            {id:3, message:'Fuck You!'},
-        ],
-        dialogs : [
-            {id:1, name:'Igor'},
-            {id:2, name:'Max'},
-            {id:3, name:'Valera'},
-            {id:4, name:'Kate'},
-            {id:5, name:'Maxon'},
-            {id:6, name:'Jane'},
-            {id:7, name:'Kill'},
-            {id:8, name:'San'},
-            {id:9, name:'Vasia'},
-            {id:10, name:'Gopnik'},
-        ]
+    getState(){
+        return this._state
     },
-    sidebar: {}
+    _rerendeerEntireTree () {
+        console.log('State change')
+    },
+    addPost () {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.postData.push(newPost);
+        this._state.profilePage.postData.newPostText = '';
+        this._rerendeerEntireTree(this._state);
+    },
+    updateNewPostText (newText) {
+        this._state.profilePage.newPostText = newText;
+        this._rerendeerEntireTree(this._state);
+    },
+    subscribe(observer) {
+        this._rerendeerEntireTree = observer;
+    }
 };
 
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
+window.store = store;
 
-    state.profilePage.postData.push(newPost);
-    rerendeerEntireTree(state);
-};
-
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerendeerEntireTree(state);
-};
-
-export  default state;
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import * as serviceWorker from './serviceWorker';
 import './index.css';
-import state from "./Redux/state";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {addPost, updateNewPostText} from './Redux/state';
+import store from './Redux/state';
 
 
 let rerendeerEntireTree = (state) => {
     ReactDOM.render(<App
         vasia={state}
-        addPost={addPost}
-        updateNewPostText={updateNewPostText}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
     />, document.getElementById('root'));
 };
+rerendeerEntireTree(store.getState());
+
+store.subscribe(rerendeerEntireTree);
 // If you want your app to work offlisdfdfsfsdne and load faster, you can change
 // unregister() to register() below. Nodfdfgdhkjhkjfgdfte this comes witassfsdfsdh some pitfalls.
 // Learn more aboutssdfsdf service workers: http://bit.ly/CRA-PWAsdfsdfsdfsdfsdfsd
